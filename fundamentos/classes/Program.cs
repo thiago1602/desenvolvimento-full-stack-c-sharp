326702d923490f26726e4e58a2ed8fc02dc09bf1
Thiago Moraes <thiagomoraesf95@gmail.com>
2021-12-27 22:33:16 -0300

    classes e metodos

---
+++ fundamentos/classes/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Classes
+{
+    class Aluno
+    {
+        public string Nome;
+        public int Idade;
+
+    }
+
+
+class Program{
+    static void Main(string[] args)
+    {
+        var aluno1 = new Aluno();
+        aluno1.Nome = "Thiago";
+        aluno1.Idade = 26;
+        
+
+
+        Console.WriteLine(aluno1.Nome);
+        Console.WriteLine(aluno1.Idade);
+
+    }
+}
+}
